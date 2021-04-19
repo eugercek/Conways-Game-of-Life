d311288ba12aac643a1dd1760a36ec6b9c6026e5
EminUmutGercek <umutgercek1999@gmail.com>
2021-04-19 20:38:53 +0300

    Format code
    Can format with C-d (Control-d).
    Currently using default settings for jedi format.

--- Project1.pas
+++ Project1.pas
@@ -4,41 +4,42 @@ type
   matrix = array of array of char;
 
 
-procedure  display(Arr: matrix);
-var
-  i, j: Integer;
-begin
-  for i:=Low(Arr) to High(Arr) do
+  procedure display(Arr: matrix);
+  var
+    i, j: integer;
   begin
-    for j:=Low(Arr[i]) to High(Arr[i]) do
-      Write(Arr[i][j]:2, '  ');
-    WriteLn;
+    for i := Low(Arr) to High(Arr) do
+    begin
+      for j := Low(Arr[i]) to High(Arr[i]) do
+        Write(Arr[i][j]: 2, '  ');
+      WriteLn;
+    end;
   end;
-end;
 
-procedure  seed(Arr: matrix);
+  procedure seed(Arr: matrix);
+  var
+    i, j, temp: integer;
+  begin
+    for i := Low(Arr) to High(Arr) do
+      for j := Low(Arr[i]) to High(Arr[i]) do
+      begin
+        temp := random(2);
+        if temp = 0 then
+          Arr[i][j] := ' '
+        else
+          Arr[i][j] := 'x';
+      end;
+
+  end;
+
 var
-  i, j, temp: Integer;
-begin
-  for i:=Low(Arr) to High(Arr) do
-    for j:=Low(Arr[i]) to High(Arr[i]) do
-        begin
-             temp := random(2);
-             if temp = 0 then
-              Arr[i][j] := ' '
-             else
-              Arr[i][j] := 'x'
-        end;
-
-end;
-
-var
-   arr : matrix;
+  arr: matrix;
 
 begin
-     Randomize; // It's like random(time(NULL)) in C
-     SetLength(arr, 5, 3);
-     seed(arr);
-     display(arr);
+  Randomize; // It's like random(time(NULL)) in C
+  SetLength(arr, 5, 3);
+  seed(arr);
+  display(arr);
 end.
+
 
