6e90c4fdd7d2f6d8f4d68ad31d74a55b2a78c6af
EminUmutGercek <umutgercek1999@gmail.com>
2021-04-19 20:16:04 +0300

    Add display

--- Project1.pas
+++ Project1.pas
@@ -4,6 +4,18 @@ type
   matrix = array of array of char;
 
 
+procedure  display(Arr: matrix);
+var
+  i, j: Integer;
+begin
+  for i:=Low(Arr) to High(Arr) do
+  begin
+    for j:=Low(Arr[i]) to High(Arr[i]) do
+      Write(Arr[i][j]:2, '  ');
+    WriteLn;
+  end;
+end;
+
 procedure  seed(Arr: matrix);
 var
   i, j, temp: Integer;
@@ -27,6 +39,6 @@ begin
      Randomize; // It's like random(time(NULL)) in C
      SetLength(arr, 5, 3);
      seed(arr);
-
+     display(arr);
 end.
 
