11f15b921c22fee8585af3abd373c8a20e335b03
EminUmutGercek <umutgercek1999@gmail.com>
2021-04-21 00:36:43 +0300

    Add GUI part skeleton and board

---
+++ unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    StringGrid1: TStringGrid;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  StringGrid1.Cells[3, 3] := 'x';
+end;
+
+end.
+
